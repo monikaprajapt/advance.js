db2f3b644516bc87059fc664541770a1e51e053f
monikaprajapt <92983459+monikaprajapt@users.noreply.github.com>
2022-02-28 15:34:30 +0530

    Add files via upload

---
+++ foreach_ma.js
@@ -0,0 +1,10 @@
+
+myfavpro=["m","o","n","i","k","a"]
+myfavpro.forEach(function(value,index){
+    console.log(index+"---"+value)
+});
+
+myfavpro.forEach(function(value,index,arr){
+    console.log(arr[index]+100)
+              
+})
